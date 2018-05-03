75d6a08fce23628ddc8a8d7a695fdf59e2d2edaf
Martin Pitt <martin@piware.de>
2018-05-03 22:41:25 +0200

    Initial podman GetVersion call
    Do a minimal renaming/retitling from StarterKit to "Podman Varlink
    Demo".
    
    Show the io.projectatomic.podman.GetVersion result on the page.

--- src/starter-kit.jsx
+++ src/starter-kit.jsx
@@ -21,21 +21,63 @@
 import cockpit from 'cockpit';
 import React from 'react';
 
+/***
+ * varlink protocol helpers
+ * https://github.com/varlink/varlink.github.io/wiki
+ */
+
+const encoder = cockpit.utf8_encoder();
+const decoder = cockpit.utf8_decoder(true);
+
+function varlinkCall(channel, method, parameters) {
+    return new Promise((resolve, reject) => {
+        function on_message(event, data) {
+            channel.removeEventListener("message", on_message);
+
+            // FIXME: support answer in multiple chunks until null byte
+            if (data[data.length - 1] != 0) {
+                reject("protocol error: expecting terminating 0");
+                return;
+            }
+
+            var reply = decoder.decode(data.slice(0, -1));
+            var json = JSON.parse(reply);
+            if (json.parameters)
+                resolve(json.parameters)
+            else if (json.error)
+                reject(json.error)
+            else
+                reject("protocol error: reply has neither parameters nor error: " + reply);
+        }
+
+        channel.addEventListener("message", on_message);
+        channel.send(encoder.encode(JSON.stringify({ method, parameters: (parameters || {}) })));
+        channel.send([0]); // message separator
+    });
+}
+
+
 export class StarterKit extends React.Component {
     constructor() {
         super();
 
-        cockpit.file('/etc/hostname').read().done((content) => {
-            this.setState({ 'hostname': content.trim() });
+        let podman = cockpit.channel({
+            payload: "stream",
+            unix: "/run/io.projectatomic.podman",
+            binary: true,
         });
+
+        varlinkCall(podman, "io.projectatomic.podman.GetVersion")
+            .then(reply => this.setState({ version: reply.version }))
+            .catch(ex => console.error("Failed to do GetVersion call:", JSON.stringify(ex)));
     }
 
     render() {
         return (
             <div className="container-fluid">
-                <h2>Starter Kit</h2>
+                <h2>Podman Varlink Demo</h2>
                 <div>
-                    <span>Running on {this.state.hostname}</span>
+                    <span>podman version: {this.state.version ? this.state.version.version : "unknown"}</span>
                 </div>
             </div>
         );
